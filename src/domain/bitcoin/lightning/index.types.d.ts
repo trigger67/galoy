3b3f9acc5394417b3d7f17942c2aeadbb8719927
vindard <17693119+vindard@users.noreply.github.com>
2021-09-30 11:26:16 -0400

    Return an error if fee probe can't find a route

--- src/domain/bitcoin/lightning/index.types.d.ts
+++ src/domain/bitcoin/lightning/index.types.d.ts
@@ -2,6 +2,7 @@ type LightningError = import("./errors").LightningError
 type LnInvoiceDecodeError = import("./errors").LnInvoiceDecodeError
 type LightningServiceError = import("./errors").LightningServiceError
 type NoValidNodeForPubkeyError = import("./errors").NoValidNodeForPubkeyError
+type RouteNotFoundError = import("./errors").RouteNotFoundError
 
 declare const invoiceExpirationSymbol: unique symbol
 type InvoiceExpiration = Date & { [invoiceExpirationSymbol]: never }
